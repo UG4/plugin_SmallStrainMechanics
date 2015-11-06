f250741c5a89e696269ebd3f451546bc330854f5
Sebastian Reiter <sebastian.reiter@gcsc.uni-frankfurt.de>
2015-11-06 13:32:24 +0000

    Added lgpl license headers to ugbase and to some plugins

--- small_strain_mech.h
+++ small_strain_mech.h
@@ -1,8 +1,33 @@
 /*
- * small_strain_mech.h
- *
- *  Created on: 16.05.2012
- *      Author: raphaelprohl, Andreas Vogel
+ * Copyright (c) 2012-2015:  G-CSC, Goethe University Frankfurt
+ * Authors: Raphael Prohl, Andreas Vogel
+ * 
+ * This file is part of UG4.
+ * 
+ * UG4 is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License version 3 (as published by the
+ * Free Software Foundation) with the following additional attribution
+ * requirements (according to LGPL/GPL v3 Â§7):
+ * 
+ * (1) The following notice must be displayed in the Appropriate Legal Notices
+ * of covered and combined works: "Based on UG4 (www.ug4.org/license)".
+ * 
+ * (2) The following notice must be displayed at a prominent place in the
+ * terminal output of covered works: "Based on UG4 (www.ug4.org/license)".
+ * 
+ * (3) The following bibliography is recommended for citation and must be
+ * preserved in all covered files:
+ * "Reiter, S., Vogel, A., Heppner, I., Rupp, M., and Wittum, G. A massively
+ *   parallel geometric multigrid solver on hierarchically distributed grids.
+ *   Computing and visualization in science 16, 4 (2013), 151-164"
+ * "Vogel, A., Reiter, S., Rupp, M., NÃ¤gel, A., and Wittum, G. UG4 -- a novel
+ *   flexible software system for simulating pde based models on high performance
+ *   computers. Computing and visualization in science 16, 4 (2013), 165-179"
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
  */
 
 #ifndef SMALL_STRAIN_MECH_H_
@@ -64,7 +89,7 @@ namespace SmallStrainMechanics{
 * <li> Elastizitaetstheorie, Springer
 * <li>
 * <li> R. Prohl. Ein verallgemeinerter Plastizitaetsalgorithmus zur numerischen Behandlung
-* von elasto-plastischen Materialmodellen unter gro§en Deformationen. Dissertation, UniversitŠt Frankfurt (2015).
+* von elasto-plastischen Materialmodellen unter groï¿½en Deformationen. Dissertation, Universitï¿½t Frankfurt (2015).
 * <li>
 * <li> M. Rupp. Berechnung der Resonanzschwingungen einer Gitarrendecke.
 * <li> (Diplomarbeit, 2009, Universitaet Heidelberg)
